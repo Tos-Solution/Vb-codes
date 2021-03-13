d0cdbbd1086297b2199e2fcb41c8a362c4b53a46
kjctos <79960783+kjctos@users.noreply.github.com>
2021-03-13 22:03:46 +0530

    Add files via upload

---
+++ abstract class/abstract class/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim s As New AB
+        s.add()
+        s.divide()
+    End Sub
+End Class
+Public MustInherit Class abstractclass
+    Public MustOverride Function add() As Integer
+    Public MustOverride Function divide() As Integer
+End Class
+Public Class AB
+    Inherits abstractclass
+    Dim x, y As UInteger
+    Public Overrides Function add() As Integer
+        x = 12
+        y = 23
+        MessageBox.Show("The sum is " & x + y)
+    End Function
+    Public Overrides Function divide() As Integer
+        x = 12
+        y = 23
+        MessageBox.Show("The division is " & x / y)
+    End Function
+End Class
